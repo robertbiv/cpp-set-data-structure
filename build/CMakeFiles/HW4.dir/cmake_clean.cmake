2023c34984fd9ebefb5a25e6d9487c13aefed02e
robertbiv <robert@robertb.me>
2025-11-14 21:05:24 +0000

    Initial plan for creating README

---
+++ build/CMakeFiles/HW4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW4.dir/link.d"
+  "CMakeFiles/HW4.dir/set.cpp.o"
+  "CMakeFiles/HW4.dir/set.cpp.o.d"
+  "HW4"
+  "HW4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
